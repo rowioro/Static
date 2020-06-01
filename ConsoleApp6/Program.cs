4ef1e934cc7ce0ff86446b0851be7cdb65c71235
Robert Stefaniak <01138659@pw.edu.pl>
2020-06-01 19:04:34 +0200

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CzlowiekStatic czlowiek1 = new CzlowiekStatic();
+            Console.WriteLine(czlowiek1.imie);
+            czlowiek1.imie = "Alicja";
+            Console.WriteLine(czlowiek1.imie);
+            Console.WriteLine(CzlowiekStatic.nazwisko);
+            CzlowiekStatic.nazwisko = "Strugala";
+            Console.WriteLine(CzlowiekStatic.nazwisko);
+            CzlowiekStatic czlowiek2 = new CzlowiekStatic();
+            Console.WriteLine(czlowiek2.imie);
+            Console.ReadKey();
+        }
+    }
+}
